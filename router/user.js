cd42763d5997d538f5e9c61a5d0ded457c1c6621
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-24 21:55:37 +0600

    Add route to fetch user's enrolled courses

--- router/user.js
+++ router/user.js
@@ -1,9 +1,11 @@
 const { userUpdate, userDelete, updateAvatar } = require("../controller/user");
 const authMiddleware = require("../middleware/authenticate/authMiddleware");
+const { enrolledCourses } = require("../controller/courseController");
 const router = require("express").Router();
 
 router.put("/update/:id", authMiddleware, userUpdate);
 router.patch("/upldate-avatar", authMiddleware, updateAvatar)
 router.delete("/delete/:id", authMiddleware, userDelete);
+router.get("/enrolled-courses", authMiddleware, enrolledCourses);
 
 module.exports = router;
