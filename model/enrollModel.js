e0975db2da8b248fde3cd2295ae3e85e15e92436
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-24 21:55:14 +0600

    Add enrolled courses route and update course reference in enrollment model

--- model/enrollModel.js
+++ model/enrollModel.js
@@ -4,7 +4,7 @@ const enrollmentSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   course: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
+    ref: "CourseModel",
     required: true,
   },
   sessionId: { type: String, required: true },
